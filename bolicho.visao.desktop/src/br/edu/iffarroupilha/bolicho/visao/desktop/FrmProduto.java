eb0aab73e90c85e2f653d2745074e4660335cce9
Lucasvieira1975 <lucasschmittvieira@gmail.com>
2017-04-11 20:50:26 -0300

    Tarefa Atualizada

--- bolicho.visao.desktop/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmProduto.java
+++ bolicho.visao.desktop/src/br/edu/iffarroupilha/bolicho/visao/desktop/FrmProduto.java
@@ -1,24 +1,31 @@
 package br.edu.iffarroupilha.bolicho.visao.desktop;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import br.edu.iffarroupilha.bolicho.controle.ProdutoControle;
+import br.edu.iffarroupilha.bolicho.modelo.Cliente;
+import br.edu.iffarroupilha.bolicho.modelo.Produto;
 
 /**
  * <p>
  * classe que representa a interface para produto.
  * </p>
- * @author Aluno_2
+ * @author fernando
  *
  */
-public class FrmProduto  extends JFrame{
+public class FrmProduto  extends JFrame {
 
 	public FrmProduto(Bolicho principal) {
 		setTitle("Bolicho -> Produto");
-		setSize(300,240);
+		setSize(320,240);
 		setLocationRelativeTo(principal);
 		desenhaComponentes();
 		setVisible(true);
@@ -26,16 +33,43 @@ public class FrmProduto  extends JFrame{
 
 	private void desenhaComponentes() {
 		// cria os componentes para a tela
-		JLabel jblDescricao = new JLabel("Descricao");
-		JLabel jblPreco = new JLabel("Preco");
-		JTextField jtfDescricao =new JTextField(30);
-		JTextField jtfPreco = new JTextField(30);
+		JLabel jblDescricao = new JLabel("Descricao:");
+		JLabel jblPreco = new JLabel("Preço:");
+		final JTextField jtfDescricao = new JTextField(30);
+		final JTextField jtfPreco = new JTextField(30);
 		JButton btnGravar = new JButton("Gravar");
 		
-		setLayout(new FlowLayout());
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		btnGravar.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				ProdutoControle controle = new ProdutoControle();				
+				// pegar os dados da tela
+				Produto p = new Produto();
+				p.setDescricao(jtfDescricao.getText() );
+				float preco = controle.converteFlutuante(jtfPreco.getText());
+				p.setPreco(preco);
+				
+				// valida os dados
+				
+				String erro = controle.validarProduto(p);
+				if (erro == null) {
+					// realiza gravação
+					controle.gravar(p);
+					
+					JOptionPane.showMessageDialog(null, "Feitooooo!!!");
+				} else{
+					JOptionPane.showMessageDialog(null, erro, "Validação", JOptionPane.WARNING_MESSAGE);
+				}
+				
+				
+			}
+		});
 		
 		// adiciona em tela
-		
 		add(jblDescricao);
 		add(jtfDescricao);
 		add(jblPreco);
